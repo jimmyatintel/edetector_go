be48c9498bc9be63e9853154e3fe6482ed5b4d5f
moirachen1019 <peggy10191019@gmail.com>
2023-07-04 15:25:01 +0800

    fix miss merge

--- internal/work_from_api/changedetectmode.go
+++ internal/work_from_api/changedetectmode.go
@@ -1,22 +1,21 @@
 package workfromapi
 
 import (
-	clientsearchsend "edetector_go/internal/clientsearch/send"
-	"edetector_go/internal/packet"
-	"edetector_go/internal/task"
-	"edetector_go/pkg/logger"
-	"encoding/json"
-	"net"
+    // clientsearchsend "edetector_go/internal/clientsearch/send"
+    "edetector_go/internal/packet"
+    "edetector_go/internal/task"
+    "edetector_go/pkg/logger"
+    "net"
 
-	// "go.uber.org/zap"
+    "go.uber.org/zap"
 )
 
 func ChangeDetectMode(p packet.UserPacket, Key *string, conn net.Conn) (task.TaskResult, error) {
   
-	// "0|0"
-	logger.Info("ChangeDetectMode: ", zap.Any("message", p.GetMessage()))
-	
+    // "0|0"
+    logger.Info("ChangeDetectMode: ", zap.Any("message", p.GetMessage()))
+    
 
 
-	return task.SUCCESS, nil
+    return task.SUCCESS, nil
 }
