6794b80c454c74c56dc8d420ce3f123e520fdb90
moirachen1019 <peggy10191019@gmail.com>
2023-08-09 10:01:48 +0800

    fix viper bug

--- config/config.go
+++ config/config.go
@@ -12,16 +12,7 @@ var Viper *viper.Viper
 // Config is a struct that holds the configuration for the application
 func LoadConfig() *viper.Viper {
 	vp := viper.New()
-	vp.SetConfigName("app")
-	vp.SetConfigType("env")
-	vp.AddConfigPath("config")
 	vp.AutomaticEnv()
-	if err := vp.ReadInConfig(); err == nil {
-		logger.Debug("Using config file:", zap.Any("config", vp.ConfigFileUsed()))
-		Viper = vp
-		return vp
-	} else {
-		logger.Error("Error loading config file:", zap.Any("error", err))
-		return nil
-	}
+	Viper = vp
+	return vp
 }
