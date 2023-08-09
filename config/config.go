166ffae18781d1f7c796a83c51879cc7d15299ee
moirachen1019 <peggy10191019@gmail.com>
2023-08-09 10:02:15 +0800

    fix viper bug

--- config/config.go
+++ config/config.go
@@ -1,10 +1,7 @@
 package config
 
 import (
-	"edetector_go/pkg/logger"
-
 	"github.com/spf13/viper"
-	"go.uber.org/zap"
 )
 
 var Viper *viper.Viper
@@ -13,12 +10,6 @@ var Viper *viper.Viper
 func LoadConfig() *viper.Viper {
 	vp := viper.New()
 	vp.AutomaticEnv()
-	if err := vp.ReadInConfig(); err == nil {
-		logger.Debug("Using config file:", zap.Any("config", vp.ConfigFileUsed()))
-		Viper = vp
-		return vp
-	} else {
-		logger.Error("Error loading config file:", zap.Any("error", err))
-		return nil
-	}
+	Viper = vp
+	return vp
 }
