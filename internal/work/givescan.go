ec6ec28b746d481b3ea542e96c11add7e172e929
moirachen1019 <peggy10191019@gmail.com>
2023-08-14 13:53:17 +0800

    disable elastic

--- internal/work/givescan.go
+++ internal/work/givescan.go
@@ -1,20 +1,13 @@
 package work
 
 import (
-	"edetector_go/config"
 	clientsearchsend "edetector_go/internal/clientsearch/send"
-	"edetector_go/internal/memory"
 	"edetector_go/internal/packet"
-	"edetector_go/internal/risklevel"
 	"edetector_go/internal/task"
 	taskservice "edetector_go/internal/taskservice"
-	elasticquery "edetector_go/pkg/elastic/query"
 	"edetector_go/pkg/logger"
 	"net"
-	"strconv"
-	"strings"
 
-	"github.com/google/uuid"
 	"go.uber.org/zap"
 )
 
@@ -37,45 +30,45 @@ func GiveScanInfo(p packet.Packet, conn net.Conn) (task.TaskResult, error) {
 func GiveScan(p packet.Packet, conn net.Conn) (task.TaskResult, error) {
 	logger.Debug("GiveScan: ", zap.Any("message", p.GetRkey()+", Msg: "+p.GetMessage()))
 	// send to elasticsearch
-	lines := strings.Split(p.GetMessage(), "\n")
-	for _, line := range lines {
-		if len(line) == 0 {
-			continue
-		}
-		line = strings.ReplaceAll(line, "|", "@|@")
-		values := strings.Split(line, "@|@")
-		int_date, err := strconv.Atoi(values[1])
-		if err != nil {
-			logger.Error("Invalid date: ", zap.Any("message", values[1]))
-			int_date = 0
-		}
-		network := "true"
-		if values[15] == "null" {
-			network = "false"
-		}
-		lastElement := strings.LastIndex(line, "@|@")
-		line = line[:lastElement] + "@|@" + network + "@|@riskLevel@|@scan"
-		logger.Info("scan data", zap.Any("message", line))
-		uuid := uuid.NewString()
-		m_tmp := memory.Memory{}
-		_, err = elasticquery.StringToStruct(uuid, p.GetRkey(), line, &m_tmp)
-		if err != nil {
-			logger.Error("Error converting to struct: ", zap.Any("error", err.Error()))
-		}
-		m_tmp.RiskLevel, err = risklevel.Getriskscore(m_tmp)
-		if err != nil {
-			logger.Error("Error getting risk level: ", zap.Any("error", err.Error()))
-		}
-		line = strings.ReplaceAll(line, "riskLevel", strconv.Itoa(m_tmp.RiskLevel))
-		err = elasticquery.SendToMainElastic(uuid, config.Viper.GetString("ELASTIC_PREFIX")+"_memory", p.GetRkey(), values[0], int_date, "memory", strconv.Itoa(m_tmp.RiskLevel), "ed_mid")
-		if err != nil {
-			logger.Error("Error sending to main elastic: ", zap.Any("error", err.Error()))
-		}
-		err = elasticquery.SendToDetailsElastic(uuid, config.Viper.GetString("ELASTIC_PREFIX")+"_memory", p.GetRkey(), line, &m_tmp, "ed_mid")
-		if err != nil {
-			logger.Error("Error sending to details elastic: ", zap.Any("error", err.Error()))
-		}
-	}
+	// lines := strings.Split(p.GetMessage(), "\n")
+	// for _, line := range lines {
+	// 	if len(line) == 0 {
+	// 		continue
+	// 	}
+	// 	line = strings.ReplaceAll(line, "|", "@|@")
+	// 	values := strings.Split(line, "@|@")
+	// 	int_date, err := strconv.Atoi(values[1])
+	// 	if err != nil {
+	// 		logger.Error("Invalid date: ", zap.Any("message", values[1]))
+	// 		int_date = 0
+	// 	}
+	// 	network := "true"
+	// 	if values[15] == "null" {
+	// 		network = "false"
+	// 	}
+	// 	lastElement := strings.LastIndex(line, "@|@")
+	// 	line = line[:lastElement] + "@|@" + network + "@|@riskLevel@|@scan"
+	// 	logger.Info("scan data", zap.Any("message", line))
+	// 	uuid := uuid.NewString()
+	// 	m_tmp := memory.Memory{}
+	// 	_, err = elasticquery.StringToStruct(uuid, p.GetRkey(), line, &m_tmp)
+	// 	if err != nil {
+	// 		logger.Error("Error converting to struct: ", zap.Any("error", err.Error()))
+	// 	}
+	// 	m_tmp.RiskLevel, err = risklevel.Getriskscore(m_tmp)
+	// 	if err != nil {
+	// 		logger.Error("Error getting risk level: ", zap.Any("error", err.Error()))
+	// 	}
+	// 	line = strings.ReplaceAll(line, "riskLevel", strconv.Itoa(m_tmp.RiskLevel))
+	// 	err = elasticquery.SendToMainElastic(uuid, config.Viper.GetString("ELASTIC_PREFIX")+"_memory", p.GetRkey(), values[0], int_date, "memory", strconv.Itoa(m_tmp.RiskLevel), "ed_mid")
+	// 	if err != nil {
+	// 		logger.Error("Error sending to main elastic: ", zap.Any("error", err.Error()))
+	// 	}
+	// 	err = elasticquery.SendToDetailsElastic(uuid, config.Viper.GetString("ELASTIC_PREFIX")+"_memory", p.GetRkey(), line, &m_tmp, "ed_mid")
+	// 	if err != nil {
+	// 		logger.Error("Error sending to details elastic: ", zap.Any("error", err.Error()))
+	// 	}
+	// }
 	var send_packet = packet.WorkPacket{
 		MacAddress: p.GetMacAddress(),
 		IpAddress:  p.GetipAddress(),
