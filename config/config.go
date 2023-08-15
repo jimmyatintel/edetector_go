ecb9adc3af60369fa0b0bc18f7c2ebbcaff62132
moirachen1019 <peggy10191019@gmail.com>
2023-08-15 14:21:26 +0800

    docker new agent

--- config/config.go
+++ config/config.go
@@ -1,10 +1,7 @@
 package config
 
 import (
-	"edetector_go/pkg/logger"
-
 	"github.com/spf13/viper"
-	"go.uber.org/zap"
 )
 
 var Viper *viper.Viper
@@ -12,20 +9,7 @@ var Viper *viper.Viper
 // Config is a struct that holds the configuration for the application
 func LoadConfig() *viper.Viper {
 	vp := viper.New()
-	vp.SetConfigName("app")
-	vp.SetConfigType("env")
-	vp.AddConfigPath("config")
 	vp.AutomaticEnv()
-	if err := vp.ReadInConfig(); err == nil {
-		logger.Debug("Using config file:", zap.Any("config", vp.ConfigFileUsed()))
-		Viper = vp
-		return vp
-	} else {
-		logger.Error("Error loading config file:", zap.Any("error", err))
-		return nil
-	}
-	// vp := viper.New()
-	// vp.AutomaticEnv()
-	// Viper = vp
-	// return vp
+	Viper = vp
+	return vp
 }
