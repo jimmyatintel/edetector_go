408be55bff3ecad4e656b5f7043e8e6d5c89a2e3
moirachen1019 <peggy10191019@gmail.com>
2023-08-18 17:50:16 +0800

    detect process fragment

--- internal/work/givedetectprocess.go
+++ internal/work/givedetectprocess.go
@@ -19,10 +19,52 @@ import (
 	"go.uber.org/zap"
 )
 
+var detectMap = make(map[string](string))
+
+func GiveDetectProcessFirst(p packet.Packet, conn net.Conn) (task.TaskResult, error) {
+	logger.Info("GiveDetectProcessFirst: ", zap.Any("message", p.GetRkey()+", Msg: "+p.GetMessage()))
+	detectMap[p.GetRkey()] = p.GetMessage()
+	var send_packet = packet.WorkPacket{
+		MacAddress: p.GetMacAddress(),
+		IpAddress:  p.GetipAddress(),
+		Work:       task.DATA_RIGHT,
+		Message:    "",
+	}
+	err := clientsearchsend.SendTCPtoClient(send_packet.Fluent(), conn)
+	if err != nil {
+		return task.FAIL, err
+	}
+	return task.SUCCESS, nil
+}
+
+func GiveDetectProcessFragment(p packet.Packet, conn net.Conn) (task.TaskResult, error) {
+	logger.Info("GiveDetectProcessFragment: ", zap.Any("message", p.GetRkey()+", Msg: "+p.GetMessage()))
+	detectMap[p.GetRkey()] += p.GetMessage()
+	var send_packet = packet.WorkPacket{
+		MacAddress: p.GetMacAddress(),
+		IpAddress:  p.GetipAddress(),
+		Work:       task.DATA_RIGHT,
+		Message:    "",
+	}
+	err := clientsearchsend.SendTCPtoClient(send_packet.Fluent(), conn)
+	if err != nil {
+		return task.FAIL, err
+	}
+	return task.SUCCESS, nil
+}
+
 func GiveDetectProcess(p packet.Packet, conn net.Conn) (task.TaskResult, error) {
-	logger.Info("GiveDetectProcess: ", zap.Any("message", p.GetRkey()+", Msg: "+p.GetMessage()))
+	key := p.GetRkey()
+	logger.Info("GiveDetectProcess: ", zap.Any("message", key+", Msg: "+p.GetMessage()))
+	_, exists := detectMap[key]
+	if !exists {
+		detectMap[key] = ""
+	}
+	detectMap[key] += p.GetMessage()
 	// send to elasticsearch
-	lines := strings.Split(p.GetMessage(), "\n")
+	lines := strings.Split(detectMap[key], "\n")
+	detectMap[key] = ""
+	// send to elasticsearch
 	for _, line := range lines {
 		if len(line) == 0 {
 			continue
