7dfb8ae68033b53e8598910ad8ed758aa2786fec
moirachen1019 <peggy10191019@gmail.com>
2023-08-24 10:51:01 +0800

    uncomment functions

--- internal/work/givedetectnetwork.go
+++ internal/work/givedetectnetwork.go
@@ -1,12 +1,18 @@
 package work
 
 import (
+	"edetector_go/config"
 	clientsearchsend "edetector_go/internal/clientsearch/send"
+	"edetector_go/internal/memory"
 	packet "edetector_go/internal/packet"
 	task "edetector_go/internal/task"
+	elasticquery "edetector_go/pkg/elastic/query"
 	"edetector_go/pkg/logger"
+	"edetector_go/pkg/mariadb/query"
 	"net"
+	"strings"
 
+	"github.com/google/uuid"
 	"go.uber.org/zap"
 )
 
@@ -27,21 +33,21 @@ func GiveDetectNetwork(p packet.Packet, conn net.Conn) (task.TaskResult, error) 
 }
 
 func detectNetworkElastic(p packet.Packet) {
-	// networkSet := make(map[string]struct{})
-	// lines := strings.Split(p.GetMessage(), "\n")
-	// for _, line := range lines {
-	// 	if len(line) == 0 {
-	// 		continue
-	// 	}
-	// 	line = strings.ReplaceAll(line, "|", "@|@")
-	// 	uuid := uuid.NewString()
-	// 	values := strings.Split(line, "@|@")
-	// 	key := values[0] + "," + values[3]
-	// 	networkSet[key] = struct{}{}
-	// 	err := elasticquery.SendToDetailsElastic(uuid, config.Viper.GetString("ELASTIC_PREFIX")+"_memory_network_detect", p.GetRkey(), line, &(memory.MemoryNetworkDetect{}), "ed_mid", "0", 0, "0", "0")
-	// 	if err != nil {
-	// 		logger.Error("Error sending to details elastic: ", zap.Any("error", err.Error()))
-	// 	}
-	// }
-	// elasticquery.UpdateNetworkInfo(p.GetRkey(), networkSet)
+	ip, name := query.GetMachineIPandName(p.GetRkey())
+	networkSet := make(map[string]struct{})
+	lines := strings.Split(p.GetMessage(), "\n")
+	for _, line := range lines {
+		if len(line) == 0 {
+			continue
+		}
+		uuid := uuid.NewString()
+		values := strings.Split(line, "|")
+		key := values[0] + "," + values[3]
+		networkSet[key] = struct{}{}
+		err := elasticquery.SendToDetailsElastic(config.Viper.GetString("ELASTIC_PREFIX")+"_memory_network_detect", &(memory.MemoryNetworkDetect{}), values, uuid, key, ip, name, "0", "0", "0", "0", "ed_mid")
+		if err != nil {
+			logger.Error("Error sending to details elastic: ", zap.Any("error", err.Error()))
+		}
+	}
+	elasticquery.UpdateNetworkInfo(p.GetRkey(), networkSet)
 }
