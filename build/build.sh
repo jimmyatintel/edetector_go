bfa1d8cf2c6d802f3469d536eca1588363da5d1d
moirachen1019 <peggy10191019@gmail.com>
2023-08-30 14:49:03 +0800

    update build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cd ..
 go build -o build/server_$1.exe -ldflags "-X main.version=$1" ./cmd/server/working_server.go
 go build -o build/connector_$2.exe -ldflags "-X main.version=$2" ./cmd/connector/connector.go
 go build -o build/parser_$3.exe -ldflags "-X main.version=$3" ./cmd/parser/parser.go
