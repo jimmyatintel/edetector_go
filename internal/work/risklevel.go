fd40f1ce4405366f02bdfecbc8370d7a80de8c49
moirachen1019 <peggy10191019@gmail.com>
2023-10-02 15:20:54 +0800

    add risklevel log

--- internal/work/risklevel.go
+++ internal/work/risklevel.go
@@ -17,9 +17,8 @@ func Getriskscore(info Memory) (string, error) {
 	} else {
 		for _, white := range whiteList {
 			if info.ProcessName == white[0] && info.ProcessMD5 == white[1] && strings.Contains(info.DigitalSign, white[2]) && strings.Contains(realPath, white[3]) {
+				logger.Debug("Hit white list")
 				return "0", nil
-			} else {
-				logger.Info("not white list!!")
 			}
 		}
 	}
@@ -28,14 +27,12 @@ func Getriskscore(info Memory) (string, error) {
 	if err != nil {
 		logger.Error("Error loading black list" + err.Error())
 	} else {
+		logger.Info("info: " + info.ProcessName + "," + info.ProcessMD5 + "," + info.DigitalSign + "," + realPath)
 		for _, black := range blackList {
 			logger.Info("black list: " + black[0] + "," + black[1] + "," + black[2] + "," + black[3])
-			logger.Info("info: " + info.ProcessName + "," + info.ProcessMD5 + "," + info.DigitalSign + "," + realPath)
 			if info.ProcessName == black[0] && info.ProcessMD5 == black[1] && strings.Contains(info.DigitalSign, black[2]) && strings.Contains(realPath, black[3]) {
-				logger.Info("black list!!")
+				logger.Debug("Hit black list")
 				return "3", nil
-			} else {
-				logger.Info("not black list!!")
 			}
 		}
 	}
@@ -51,6 +48,7 @@ func Getriskscore(info Memory) (string, error) {
 					logger.Error("Error converting adding_point to integer" + err.Error())
 					continue
 				}
+				logger.Debug("Hit hack list")
 				score += point
 			}
 		}
