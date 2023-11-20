56960fefcdb6b7a4df0cb3b32715edeb478efbc8
moirachen1019 <peggy10191019@gmail.com>
2023-11-20 16:45:43 +0800

    remove pprof

--- cmd/server/working_server.go
+++ cmd/server/working_server.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	server "edetector_go/internal/server"
-	pprof_test "edetector_go/pkg/pprof"
 )
 
 var version string
 
 func main() {
-	go pprof_test.Pprof_service()
+	// go pprof_test.Pprof_service()
 	server.Main(version, nil)
 	// stop.Stop()
 }
