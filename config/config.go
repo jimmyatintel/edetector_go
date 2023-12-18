117934618c14b3e5e24316db814c3d402affa0eb
moirachen1019 <peggy10191019@gmail.com>
2023-12-18 17:46:15 +0800

    local version

--- config/config.go
+++ config/config.go
@@ -9,6 +9,9 @@ var Viper *viper.Viper
 // Config is a struct that holds the configuration for the application
 func LoadConfig() (*viper.Viper, error) {
 	vp := viper.New()
+	vp.SetConfigName("app")
+	vp.SetConfigType("env")
+	vp.AddConfigPath("config")
 	vp.AutomaticEnv()
 	if err := vp.ReadInConfig(); err != nil {
 		return nil, err
