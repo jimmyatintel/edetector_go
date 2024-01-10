2eda446456cc96ea9cc7e7f89ebffc3abe5df13f
moirachen1019 <peggy10191019@gmail.com>
2024-01-10 06:18:27 +0000

    send path to agent

--- internal/work/giverulematch.go
+++ internal/work/giverulematch.go
@@ -8,72 +8,12 @@ import (
 	"edetector_go/pkg/logger"
 	"edetector_go/pkg/mariadb/query"
 	"edetector_go/pkg/redis"
-	"math"
 	"net"
 	"os"
 	"path/filepath"
 	"strconv"
 	"strings"
 )
-
-var srcPath = filepath.Join("static", "yaraRule")
-var dstPath = filepath.Join("static", "yaraRule.zip")
-var ruleMatchWorkingPath = "ruleMatchWorking"
-var ruleMatchUnstage = "ruleMatchUnstage"
-
-func init() {
-	file.CheckDir(ruleMatchWorkingPath)
-	file.CheckDir(ruleMatchUnstage)
-}
-
-func ReadyYaraRule(p packet.Packet, conn net.Conn, dataRight chan net.Conn) (task.TaskResult, error) {
-	logger.Info("ReadyYaraRule: " + p.GetRkey() + "::" + p.GetMessage())
-	// zip the file
-	err := file.ZipDirectory(srcPath, dstPath)
-	if err != nil {
-		return task.FAIL, err
-	}
-	fileInfo, err := os.Stat(dstPath)
-	if err != nil {
-		return task.FAIL, err
-	}
-	fileLen := int(fileInfo.Size())
-	logger.Info("ServerSend GiveYaraRuleInfo: " + p.GetRkey() + "::" + strconv.Itoa(fileLen))
-	err = clientsearchsend.SendTCPtoClient(p, task.GIVE_YARA_RULE_INFO, strconv.Itoa(fileLen), conn)
-	if err != nil {
-		return task.FAIL, err
-	}
-	go GiveYaraRule(p, fileLen, dstPath, dataRight)
-	return task.SUCCESS, nil
-}
-
-func GiveYaraRule(p packet.Packet, fileLen int, path string, dataRight chan net.Conn) {
-	content, err := os.ReadFile(path)
-	if err != nil {
-		logger.Error("Read file error: " + err.Error())
-	}
-	start := 0
-	for {
-		conn := <-dataRight
-		if start >= fileLen {
-			logger.Info("ServerSend GiveYaraRuleEnd: " + p.GetRkey())
-			err = clientsearchsend.SendDataTCPtoClient(p, task.GIVE_YARA_RULE_END, []byte{}, conn)
-			if err != nil {
-				logger.Error("Send GiveYaraRuleEnd error: " + err.Error())
-			}
-			<-dataRight
-			break
-		}
-		end := int(math.Min(float64(fileLen), float64(start+65436)))
-		data := content[start:end]
-		logger.Info("ServerSend GiveYaraRule: " + p.GetRkey())
-		err := clientsearchsend.SendDataTCPtoClient(p, task.GIVE_YARA_RULE, data, conn)
-		if err != nil {
-			logger.Error("Send GiveYaraRule error: " + err.Error())
-		}
-		start += 65436
-	}
-}
 
 func GiveRuleMatchInfo(p packet.Packet, conn net.Conn) (task.TaskResult, error) {
 	key := p.GetRkey()
