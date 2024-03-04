18426620c40642b6b952d993590b3d001498aad6
moirachen1019 <peggy10191019@gmail.com>
2024-03-04 06:42:05 +0000

    add more time layout format

--- internal/dbparser/converttime.go
+++ internal/dbparser/converttime.go
@@ -13,9 +13,11 @@ func RFCToTimestamp(original string) string {
 	var err error
 	// list other possible layouts
 	layouts := []string{
-		"Mon, 02 Jan 2006 15:04:05 GMT", "Mon, 2 Jan 2006 15:04:05 GMT",
-		"Mon, 02 Jan 2006 15:04:05 MST", "Mon, 2 Jan 2006 15:04:05 MST",
-		"Mon, 02 Jan 2006 15:04:05 UTC", "Mon, 2 Jan 2006 15:04:05 UTC",}
+		"Mon, _2 Jan 2006 15:04:05 GMT",
+		"Mon, _2 Jan 2006 15:04:05 MST",
+		"Mon, _2 Jan 2006 15:04:05 UTC",
+		"Mon Jan _2 15:04:05 2006",
+		}
 	for _, layout := range layouts {
 		t, err = time.Parse(layout, original)
 		if err == nil {
