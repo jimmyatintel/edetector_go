59701de2f6f3d6e4d6203bda1e5308d4848ffcdb
moirachen1019 <peggy10191019@gmail.com>
2024-03-05 08:02:43 +0000

    update logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,6 +1,7 @@
 package logger
 
 import (
+	"edetector_go/config"
 	"edetector_go/pkg/mariadb"
 	"fmt"
 	"os"
@@ -9,8 +10,11 @@ import (
 	"time"
 
 	"github.com/gin-gonic/gin"
+	zapsyslog "github.com/imperfectgo/zap-syslog"
+	"github.com/imperfectgo/zap-syslog/syslog"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	lumberjack "gopkg.in/natefinch/lumberjack.v2"
 )
 
 var Log *zap.Logger
@@ -18,8 +22,15 @@ var Service string
 
 func InitLogger(path string, hostname string, app string) {
 	// file logger
-	file, _ := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
-	fileWriteSyncer := zapcore.AddSync(file)
+	// file, _ := os.OpenFile(path, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
+	// fileWriteSyncer := zapcore.AddSync(file)
+	rotateCfg := zapcore.AddSync(&lumberjack.Logger{
+		Filename:   path,
+		MaxSize:    5, // megabytes
+		MaxBackups: 10,
+		MaxAge:     30, // days
+	})
+	rotatedFileWriteSyncer := zapcore.AddSync(rotateCfg)
 	productionCfg := zap.NewProductionEncoderConfig()
 	productionCfg.EncodeTime = zapcore.ISO8601TimeEncoder
 	fileEncoder := zapcore.NewJSONEncoder(productionCfg)
@@ -29,33 +40,33 @@ func InitLogger(path string, hostname string, app string) {
 	developmentCfg.EncodeLevel = zapcore.CapitalColorLevelEncoder
 	consoleEncoder := zapcore.NewConsoleEncoder(developmentCfg)
 	// system logger
-	// syslogCfg := zapsyslog.SyslogEncoderConfig{
-	// 	EncoderConfig: zapcore.EncoderConfig{
-	// 		NameKey:        "logger",
-	// 		CallerKey:      "caller",
-	// 		MessageKey:     "msg",
-	// 		StacktraceKey:  "stacktrace",
-	// 		EncodeLevel:    zapcore.CapitalColorLevelEncoder,
-	// 		EncodeTime:     zapcore.EpochTimeEncoder,
-	// 		EncodeDuration: zapcore.SecondsDurationEncoder,
-	// 		EncodeCaller:   zapcore.ShortCallerEncoder,
-	// 	},
-	// 	Facility: syslog.LOG_LOCAL0,
-	// 	Hostname: hostname,
-	// 	PID:      os.Getpid(),
-	// 	App:      app,
-	// }
-	// syslogEncoder := zapsyslog.NewSyslogEncoder(syslogCfg)
-	// graylogPath := fmt.Sprintf("%s:%s", config.Viper.GetString("GRAYLOG_HOST"), config.Viper.GetString("GRAYLOG_SYSLOG_PORT"))
-	// sync, err := zapsyslog.NewConnSyncer("tcp", graylogPath)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
+	syslogCfg := zapsyslog.SyslogEncoderConfig{
+		EncoderConfig: zapcore.EncoderConfig{
+			NameKey:        "logger",
+			CallerKey:      "caller",
+			MessageKey:     "msg",
+			StacktraceKey:  "stacktrace",
+			EncodeLevel:    zapcore.CapitalColorLevelEncoder,
+			EncodeTime:     zapcore.EpochTimeEncoder,
+			EncodeDuration: zapcore.SecondsDurationEncoder,
+			EncodeCaller:   zapcore.ShortCallerEncoder,
+		},
+		Facility: syslog.LOG_LOCAL0,
+		Hostname: hostname,
+		PID:      os.Getpid(),
+		App:      app,
+	}
+	syslogEncoder := zapsyslog.NewSyslogEncoder(syslogCfg)
+	graylogPath := fmt.Sprintf("%s:%s", config.Viper.GetString("GRAYLOG_HOST"), config.Viper.GetString("GRAYLOG_SYSLOG_PORT"))
+	sync, err := zapsyslog.NewConnSyncer("tcp", graylogPath)
+	if err != nil {
+		fmt.Println(err)
+	}
 	// set core
 	core := zapcore.NewTee(
-		zapcore.NewCore(consoleEncoder, stdout, zapcore.DebugLevel),
-		zapcore.NewCore(fileEncoder, fileWriteSyncer, zapcore.DebugLevel),
-		// zapcore.NewCore(syslogEncoder, zapcore.Lock(sync), zapcore.DebugLevel),
+		zapcore.NewCore(consoleEncoder, stdout, zapcore.InfoLevel),
+		zapcore.NewCore(fileEncoder, rotatedFileWriteSyncer, zapcore.DebugLevel),
+		zapcore.NewCore(syslogEncoder, zapcore.Lock(sync), zapcore.InfoLevel),
 	)
 	Log = zap.New(core)
 	Service = app
@@ -139,11 +150,9 @@ func StoreLogToDB(level string, message string) {
 	if len(message) > 100 {
 		message = message[:100]
 	}
-	if Service != "MOCKAGENT" {
-		query := "INSERT INTO log (level, service, content, timestamp) VALUES (?, ?, ?, CURRENT_TIMESTAMP)"
-		_, err := mariadb.DB.Exec(query, level, Service, message)
-		if err != nil {
-			Log.Error("Error storing log to database: " + err.Error())
-		}
+	query := "INSERT INTO log (level, service, content, timestamp) VALUES (?, ?, ?, CURRENT_TIMESTAMP)"
+	_, err := mariadb.DB.Exec(query, level, Service, message)
+	if err != nil {
+		Log.Error("Error storing log to database: " + err.Error())
 	}
 }
